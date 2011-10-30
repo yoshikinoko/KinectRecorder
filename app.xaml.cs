d7dc8e81a687417271b1b80e86d99d7a6df2574b
yoshikinoko <yoshikinoko@gmail.com>
2011-10-30 20:16:04 +0900

    Initial Import

---
+++ app.xaml.cs
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////////
+//
+// Copyright © Microsoft Corporation.  All rights reserved.  
+// This code is licensed under the terms of the 
+// Microsoft Kinect for Windows SDK (Beta) from Microsoft Research 
+// License Agreement: http://research.microsoft.com/KinectSDK-ToU
+//
+/////////////////////////////////////////////////////////////////////////
+using System;
+using System.Configuration;
+using System.Windows;
+
+namespace SkeletalViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
